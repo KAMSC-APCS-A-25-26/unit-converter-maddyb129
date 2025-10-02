ba556be28bed0a681871ee581fa1bd8af03cfa2c
maddyb129 <maddyball814@gmail.com>
2025-10-02 11:27:04 -0400

    Update DistanceConverter.java

--- src/main/java/DistanceConverter.java
+++ src/main/java/DistanceConverter.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class DistanceConverter {
     public static void main(String[] args) {
         // TODO: Implement distance converter
@@ -7,5 +8,34 @@ public class DistanceConverter {
         // - Use conversion factor: 1 mile = 1.60935 kilometers
         // - Display conversion results
         // - Handle invalid menu choices
-    }
-}
+            Scanner sc = new Scanner(System.in);
+                boolean running = true;
+                while (running)
+                {
+                    System.out.println("---DistanceConverter Menu---");
+                    System.out.println("1. Miles to Kilometers");
+                    System.out.println("2. Kilometers to Miles");
+                    System.out.println("3. Exit");
+                    System.out.print("Enter your choice:  ");
+                    int choice = sc.nextInt();
+                    switch(choice){
+                        case 1:
+                            System.out.print("Enter distance (miles):  ");
+                            double miles = sc.nextDouble();
+                            double kilometers = miles * 1.60935;
+                            System.out.println(miles + " miles is equal to " + kilometers + " kilometers \n");
+                            break;
+                        case 2:
+                            System.out.print("Enter distance (kilometers):  ");
+                            double kilo = sc.nextDouble();
+                            double mil = kilo / 1.60935;
+                            System.out.println(kilo + " kilometers is equal to " + mil + " miles \n");
+                            break;
+                        case 3:
+                            System.out.println("Goodbye!");
+                            running = false;
+                            break;
+                    }
+                }
+            }
+        }
